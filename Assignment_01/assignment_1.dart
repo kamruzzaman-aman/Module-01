67d50bb9e9c63eb692fe5ccbbde46aaaac0e72be
KAMRUZZAMAN <kamrulcse1@gmail.com>
2023-08-02 20:57:24 +0600

    Assignment 01

---
+++ Assignment_01/assignment_1.dart
@@ -0,0 +1,19 @@
+void main() {
+  const a = 25;
+  const b = 18;
+  // Addition
+  int sum = a + b;
+  print('Addition: $a+ $b = $sum');
+  // Subtraction
+  int difference = a - b;
+  print('Subtraction: $a - $b = $difference');
+  //Multiplication
+  int product = a * b;
+  print('Multiplication: $a * $b = $product');
+  //Division
+  double quotient = a / b;
+  print('Division: $a / $b = $quotient');
+  //Modulus
+  int remainder = a % b;
+  print('Modulus: $a % $b = $remainder');
+}
