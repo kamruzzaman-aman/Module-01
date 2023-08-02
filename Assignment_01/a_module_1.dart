1fdf8bd8ade51c09b5ef074d4fbca27f2b04d408
KAMRUZZAMAN <kamrulcse1@gmail.com>
2023-08-02 20:41:06 +0600

    Module_01 (Assignment_01)

---
+++ Assignment_01/a_module_1.dart
@@ -0,0 +1,9 @@
+void main() {
+  const a = 25;
+  const b = 18;
+  print('Addition: ${a} + ${b} = ${a + b}');
+  print('Subtraction: ${a} - ${b} = ${a - b}');
+  print('Multiplication: ${a} * ${b} = ${a * b}');
+  print('Division: ${a} / ${b} = ${a / b}');
+  print('Modulus: ${a} % ${b} = ${a % b}');
+}
